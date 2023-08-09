5ff62093bf9bdc804ec90dd9163c5b8e3d583ac3
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-08-09 10:26:03 +0200

    add profile sites

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
+function requireLogin(req, res, next) {
+  if (!req.user) {
+    return res.redirect('/user/no-permission');
+  }
+
+  next();
+}
+
+router.get('/logged', requireLogin, (req, res) => {
+  res.render('logged', {
+    user: req.user.displayName,
+    avatar: req.user.photos[0].value,
+  });
+  console.log(req.user);
+});
+
+router.get('/profile', requireLogin, (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', requireLogin, (req, res) => {
+  res.render('settings');
 });
 
 router.get('/no-permission', (req, res) => {
